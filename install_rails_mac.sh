b49f83199a62a5041cb31347ab6826b08426027b
Simon Liang <no.lhr0909@gmail.com>
2015-12-18 18:15:00 -0800

    Comment out for installing in US

--- install_rails_mac.sh
+++ install_rails_mac.sh
@@ -13,12 +13,12 @@ brew install libxml2 libxslt libiconv
 curl -L https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
-sed -i .bak -E 's!https?://cache.ruby-lang.org/pub/ruby!https://ruby.taobao.org/mirrors/ruby!' $rvm_path/config/db
+# sed -i .bak -E 's!https?://cache.ruby-lang.org/pub/ruby!https://ruby.taobao.org/mirrors/ruby!' $rvm_path/config/db
 
 # 安装Ruby
 rvm install 2.2.4
 rvm 2.2.4 --default
 
 # RubyGems设置和安装bundler
-gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
+# gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
 gem install bundler
